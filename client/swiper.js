e68c1eeac0faf51a5a5a61d5b6f6364a3beb76ee
tae-lim <taelim92@gmail.com>
2019-03-02 16:44:39 -0800

    Implement swiping feature to skills section

---
+++ client/swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('#skills-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: '3',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows : true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+console.log(swiper);
