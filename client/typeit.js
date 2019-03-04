62051b7cc75a39a73c0209e238800d82ea01fe5b
tae-lim <taelim92@gmail.com>
2019-03-04 13:02:35 -0800

    Add pics and background colors styling

--- client/typeit.js
+++ client/typeit.js
@@ -1,5 +1,5 @@
 const instance = new TypeIt('#about-me', {
-  strings: ['Hi! My name is Tae! Nice to meet you!'],
+  strings: ['Hi! My name is Tae! Welcome to my portfolio!'],
   speed: 50,
   waitUntilVisible: true,
 }).go();
